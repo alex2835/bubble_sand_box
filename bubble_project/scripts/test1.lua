9202b1c23e959c640a07b2093b95f574d8de7f6d
Alexander Yushenko <alexander.iushchenko@glidewelldental.com>
2025-03-27 14:18:31 +0400

    new scene

--- bubble_project/scripts/test1.lua
+++ bubble_project/scripts/test1.lua
@@ -7,22 +7,23 @@
 --entity:AddTransformComponent(transform)
 
 function OnUpdate()
+    
     if bIsKeyPressed(bKeyboard.SPACE) then
-        local entity = bScene:CreateEntity()
-        print(entity)
-        entity:AddTagComponent("created in script entity")
-        local pos = vec3(math.random(1,200), 0, math.random(1,200))
-        entity:AddTransformComponent(Transform(pos, vec3(), vec3(1.0)))
-        entity:AddModelComponent(bLoader:LoadModel("models/gribok/gribok.obj"))
-        entity:AddShaderComponent(bLoader:LoadShader("./resources/shaders/only_diffuse"))
+        for i = 1,10 do
+            local entity = bScene:CreateEntity()
+            print(entity)
+            entity:AddTagComponent("created in script entity")
+            local pos = vec3(math.random(1,200), 0, math.random(1,200))
+            entity:AddTransformComponent(Transform(pos, vec3(), vec3(1.0)))
+            entity:AddModelComponent(bLoader:LoadModel("models/gribok/gribok.obj"))
+            entity:AddShaderComponent(bLoader:LoadShader("./resources/shaders/only_diffuse"))
 
-        -- local view = bScene:GetRuntimeView({"TransformComponent"})
-        -- for idx, entity in pairs(view) do
-            -- local transform = entity:GetTransformComponent()
-            -- print(entity:GetTagComponent().Name)
-            -- transform.Position.x = transform.Position.x + 0.1
-        -- end
+            -- local view = bScene:GetRuntimeView({"TransformComponent"})
+            -- for idx, entity in pairs(view) do
+                -- local transform = entity:GetTransformComponent()
+                -- print(entity:GetTagComponent().Name)
+                -- transform.Position.x = transform.Position.x + 0.1
+            -- end
+        end
     end
 end
-
-
