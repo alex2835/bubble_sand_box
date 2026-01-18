0d4ff420e90e91966e6c839b079f9c12f042b744
Alexander Yushenko <alexander.iushchenko@glidewelldental.com>
2026-01-18 20:46:22 +0400

    camera scene

--- bubble_project/scripts/shepe_character.lua
+++ bubble_project/scripts/shepe_character.lua
@@ -4,19 +4,19 @@ function OnUpdate(entity, state)
     local physics = entity:GetPhysicsComponent()
 
     if IsKeyPressed(KeyboardKey.W) then
-        physics:ApplyTorqueImpulse(vec3(0.01, 0, 0))
+        physics:ApplyTorqueImpulse(vec3(0.1, 0, 0))
     end
 
     if IsKeyPressed(KeyboardKey.S) then
-        physics:ApplyTorqueImpulse(vec3(-0.01, 0, 0))
+        physics:ApplyTorqueImpulse(vec3(-0.1, 0, 0))
     end
 
     if IsKeyPressed(KeyboardKey.D) then
-        physics:ApplyTorqueImpulse(vec3(0, 0, 0.01))
+        physics:ApplyTorqueImpulse(vec3(0, 0, 0.1))
     end
 
     if IsKeyPressed(KeyboardKey.A) then
-        physics:ApplyTorqueImpulse(vec3(0, 0, -0.01))
+        physics:ApplyTorqueImpulse(vec3(0, 0, -0.1))
     end
 
 
